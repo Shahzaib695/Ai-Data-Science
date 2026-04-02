5e8fc76b4c5edfc19e806ffae6a492f5a4e2cec0
Shahzaib Baig <78021129+Shahzaib695@users.noreply.github.com>
2026-04-02 16:04:22 +0500

    Update task1.py

--- Python-Fundamentals/Chapter2/practice sets/task1.py
+++ Python-Fundamentals/Chapter2/practice sets/task1.py
@@ -1,4 +1,5 @@
 #  Write a python program to add two numbers.
 a = int(input("Enter Number 1 To Take Sum Of: "));
+b = int(input("Enter Number 2 To Take Sum Of: "));
 sum = a+b;
 print(f"The Sum Of {a} And {b} Is {sum}");
